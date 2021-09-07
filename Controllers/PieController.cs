438d81f3e1be743463c1efee9f24ff92ce15fc3b
Onosu Great <onosugreat@gmail.com>
2021-09-07 20:58:58 +0100

    Add project files.

---
+++ Controllers/PieController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PieShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PieShop.Controllers
+{
+	public class PieController : Controller
+	{
+		private readonly IPieRepo _pieRepo;
+		private readonly ICategoryRepo _categoryRepo;
+
+		public PieController(IPieRepo pieRepo, ICategoryRepo categoryRepo) 
+		{
+			_pieRepo = pieRepo;
+			_categoryRepo = categoryRepo;
+		}
+
+		public ViewResult List()
+		{
+			ViewBag.NewCategory = "Great";
+			return View();
+		}
+		/*public IActionResult Index()
+		{
+			return View();
+		}*/
+	}
+}
